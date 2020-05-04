ec3ca5e3e3948d36d520daf5efc171437b6dd2c4
Jamy Southafeng <jsouthafeng@gmail.com>
2020-05-04 15:29:20 -0700

    Uploading entire project created from local computer

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ItemCardComponent } from './item-list/item-card/item-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    AddItemFormComponent,
+    ItemListComponent,
+    ItemCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
